c13d5fc0110237b16da45364284cc9db07e5add8
Neeraj <116136410+nrj111@users.noreply.github.com>
2024-02-01 12:00:50 +0530

    Add files via upload
    Added frontend files

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = ({isLoggedIn}) => {
+  return (
+    <div className='flex items-center justify-center
+     text-white text-3xl h-full'>
+      Home
+    </div>
+  )
+}
+
+export default Home;
